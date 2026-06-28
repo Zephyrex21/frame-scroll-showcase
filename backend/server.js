22753f26e5139d93680119234efc25e7cc1bbf15
Zephyrex21 <shekharsaurabhraj@gmail.com>
2026-06-28 11:15:01 +0530

    Updated Webapp

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin:[
         "http://localhost:5173",
-        "https://your-vercel-url.vercel.app"
+        "https://frame-scroll-showcase.vercel.app/"
     ],
   credentials: true,
 }));
